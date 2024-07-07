686fa7c3a63456be45de36d9da9d56d500820234
yangerdanger27 <93808580+yangerdanger@users.noreply.github.com>
2024-07-07 13:08:24 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Flex, Text, Button, Image } from '@aws-amplify/ui-react';
+import { ActionCard, CommentCard }  from './ui-components';
+
 
 const client = generateClient<Schema>();
 
@@ -23,25 +26,7 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          
-          <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    CommentCard
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import {ThemeProvider} from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
+import "@aws-amplify/ui-react/styles.css";
+import studioTheme from './ui-components/studioTheme';
+
+Amplify.configure(amplifyconfig);
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={studioTheme}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ThemeProvider>
 );
